a7110a6a0033152b74f3b6934f499f431a102ae4
y4maru <69639784+y4maru@users.noreply.github.com>
2020-08-18 04:16:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,16 @@
     // const results=['大吉','中吉','凶'];
     // btn.textContent=results[Math.floor(Math.random() * results.length)];
     const n =Math.random();
-    if (n<0.1){
-      btn.textcontent='まー';// 10%
-    } else if(n<0.4){
+    if (n<0.2){
+      btn.textcontent='まー';//20%
+    } else if(n<0.5){
       btn.textContent='大吉';//30%
-    } else if(n<0.9){
-      btn.textContent='中吉';//50%
+    } else if(n<0.7){
+      btn.textContent='吉';//20%
+      } else if(n<0.8){
+      btn.textContent='中吉';//10%
+      } else if(n<0.9){
+      btn.textContent='末吉';//10%
     } else{
       btn.textContent='凶';//10%
     }
