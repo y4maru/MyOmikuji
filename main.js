e37ba1b61b0782a790ba38bb5417b3dbbd653d95
y4maru <69639784+y4maru@users.noreply.github.com>
2020-08-18 04:08:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,9 +8,9 @@
     // btn.textContent=results[Math.floor(Math.random() * results.length)];
     const n =Math.random();
     if (n<0.1){
-      btn.textcontent='まー';// 5%
+      btn.textcontent='まー';// 10%
     } else if(n<0.4){
-      btn.textContent='大吉';//35%
+      btn.textContent='大吉';//30%
     } else if(n<0.9){
       btn.textContent='中吉';//50%
     } else{
